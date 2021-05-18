addad1280dc9471aa3be23347809393da97f38d8
haki9975 <elias.robert.hakim@gmail.com>
2021-05-18 15:46:04 -0400

    commit tree ee5984210dc384d9eb96dd627ce3ce2fccc63319

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+const destinationURL = "http://localhost:3000/users"
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: { 
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+
+const doc = document.getElementById("index")
+
+
+function submitData() {
+    fetch(destinationURL, configurationObject)
+        .then(function(response){
+            return response.json();
+        })
+            .then(function(object){
+                doc.append(object[id])
+            })
+                .catch(function(error){
+                    alert("Unauthorized Access")
+                    console.log(error.message)
+                })
+
+};
+
+
+
